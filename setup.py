94e5019aec6cdabe97f8498da29ee0aaa7ea30b3
Alex Knaust <awknaust@gmail.com>
2015-04-25 13:30:06 -0500

    Updated setup to include extra files. Add version info

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
+import ddgrader
 
 setup(
     name="DDGrader",
-    version=1.4,
-    packages=['ddgrader',],
+    version=ddgrader.__version__,
+    packages=['ddgrader', 'ddgrader.commands'],
     scripts=['bin/project_prepare', 'bin/ddgrader'],
     author="Alex Knaust",
     author_email="awknaust@gmail.com",
-
-
 )
